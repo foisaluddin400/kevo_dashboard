98fb424f89ce86e0bd2f9c5a91b78c05ee60be39
foisal <foisalrk2@gmail.com>
2026-03-25 13:58:35 +0600

    uyd

--- src/page/Notification/Notification.jsx
+++ src/page/Notification/Notification.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Bell, Clock, Trash2 } from "lucide-react";
 import { Navigate } from "../../Navigate";
-import LogoIco from "../../components/icon/LogoIco";
+import LogoIco from "../../assets/logo1.png";
 
 const Notification = () => {
   const [notifications, setNotifications] = useState([
@@ -38,15 +38,15 @@ const Notification = () => {
         {notifications.map((note) => (
           <div
             key={note.id}
-            className="border border-[#2A2448] rounded-lg p-3 hover:shadow-md transition duration-200"
+            className="border border-borderColor rounded-lg p-3 hover:shadow-md transition duration-200"
           >
             <div className="flex items-center gap-3  justify-between">
-             <div className="border border-[#2A2448] bg-[#822CE71A] w-[50px] h-[50px] rounded-xl flex items-center justify-center">
-               <LogoIco></LogoIco>
+             <div className="border border-borderColor bg-[#2B7FFF1A] w-[50px] h-[50px] rounded-xl flex items-center justify-center">
+               <img src={LogoIco} alt="" />
              </div>
               <div className="flex-1">
                 <div className="flex items-center justify-between mb-1">
-                  <h3 className="text-lg font-semibold text-white italic">{note.title}</h3>
+                  <h3 className="text-lg font-semibold  italic">{note.title}</h3>
                   <div className="flex items-center gap-1 text-sm text-gray-500">
                     <Clock className="w-4 h-4" />
                     {note.time}
